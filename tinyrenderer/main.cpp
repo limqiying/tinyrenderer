dae5033fcb7f47040f048a15db67d62369935ad5
Tia <limqiying19@gmail.com>
2019-10-19 17:40:08 -0400

    Initial Commit

---
+++ tinyrenderer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tinyrenderer
+//
+//  Created by Qi Ying Lim on 10/19/19.
+//  Copyright © 2019 Tia Lim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
