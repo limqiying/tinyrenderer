e5f1b7ffe9ffadcdf2792271312e1cf8e7f3d6b8
Tia Lim <limqiying19@gmail.com>
2019-11-03 17:41:56 -0500

    add triangle drawing function

--- include/utils.h
+++ include/utils.h
@@ -41,7 +41,7 @@ void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &c
 
 void drawLine(const Eigen::Vector2i &p1, const Eigen::Vector2i &p2, TGAImage &image, const TGAColor &color) 
 {
-    drawLine(p1.x(), p1.y(), p2.x(), p2.y(), image, color);
+    drawLine(p1[0], p1[1], p2[0], p2[1], image, color);
 }
 
 
@@ -59,8 +59,8 @@ void drawWireFrameMesh(const char* inputfile, const char* outputfile, int width,
     // this is used to color the lines
     float minZ, maxZ;
     for (int i=0; i<model ->nverts(); i++) {
-        minZ = std::min(minZ, model->vert(i).z());
-        maxZ = std::max(minZ, model->vert(i).z());
+        minZ = std::min(minZ, model->vert(i)[2]);
+        maxZ = std::max(minZ, model->vert(i)[2]);
     }
 
     for (int i=0; i<model->nfaces(); i++) {
@@ -75,13 +75,13 @@ void drawWireFrameMesh(const char* inputfile, const char* outputfile, int width,
             // assuming that model is centered at (0, 0)
             // shift vertex value from [-1, 1] to [0, 1]
             // scale to [0, height] / [0, width]
-            x0 = ((v0.x() + 1.0) / 2.0) * width ;
-            y0 = ((v0.y() + 1.0) / 2.0) * height;
-            x1 = ((v1.x() + 1.0) / 2.0) * width ;
-            y1 = ((v1.y() + 1.0) / 2.0) * height;
+            x0 = ((v0[0] + 1.0) / 2.0) * width ;
+            y0 = ((v0[1] + 1.0) / 2.0) * height;
+            x1 = ((v1[0] + 1.0) / 2.0) * width ;
+            y1 = ((v1[1] + 1.0) / 2.0) * height;
 
             // draw the line
-            const float z = (1.0 - ((std::min(v0.z(), v1.z()) + std::abs(minZ)) / (maxZ - minZ))) * 255;
+            const float z = (1.0 - ((std::min(v0[2], v1[2]) + std::abs(minZ)) / (maxZ - minZ))) * 255;
             const TGAColor lineColor = TGAColor(z, z, z, 255);
             drawLine(x0, y0, x1, y1, image, lineColor);
         }
@@ -92,37 +92,67 @@ void drawWireFrameMesh(const char* inputfile, const char* outputfile, int width,
 
 }
 
-Eigen::Vector3f getBarycentricCoordinates(const Eigen::Vector2i *triangle, const Eigen::Vector2i &point)
+void getBarycentricCoordinates(const Eigen::Vector2i *triangle, const Eigen::Vector2i &point, Eigen::Vector3f &barycentric)
 {
     // returns the barycentric coordinate of the point in terms of the input triangle
-    Eigen::Vector3f u = Eigen::Vector3f(triangle[2].x() - triangle[0].x(), triangle[1].x() - triangle[0].x(), triangle[0].x() - point.x())
-    .cross(Eigen::Vector3f(triangle[2].y() - triangle[0].y(), triangle[1].y() - triangle[0].x(), triangle[0].y() - point.y()));
+    Eigen::Vector3f u = Eigen::Vector3f(triangle[2][0]-triangle[0][0], triangle[1][0]-triangle[0][0], triangle[0][0]-point[0])
+    .cross(Eigen::Vector3f(triangle[2][1]-triangle[0][1], triangle[1][1]-triangle[0][1], triangle[0][1]-point[1]));
 
-    if (std::abs(u.z())<1) {
-        return Eigen::Vector3f(-1,1,1);
+    if (std::abs(u[2])<1) {
+        barycentric[0] = -1.0;
+        barycentric[1] = 1.0;
+        barycentric[2] = 1.0;
+    } else {
+        // solution using cramer's rule to find coordinates
+        barycentric[0] = 1.0 - ((u[0] + u[1]) / u[2]);
+        barycentric[1] = u[1] / u[2];
+        barycentric[2] = u[0] / u[2];
     }
-
-    // solution using cramer's rule to find coordinates
-    return Eigen::Vector3f(1.0 - (u.x() + u.y()) / u.z(), u.y() / u.z(), u.x() / u.z());
 }
 
-bool pointInTriangle(const Eigen::Vector2i *triangle, const Eigen::Vector2i &point)
+bool pointInTriangle(const Eigen::Vector2i *triangle, const Eigen::Vector2i &point, Eigen::Vector3f &barycentric)
 {
-    Eigen::Vector3f barycentric = getBarycentricCoordinates(triangle, point);
-    return ! (barycentric.x() < 0 || barycentric.y() < 0 || barycentric.z() < 0);
+    getBarycentricCoordinates(triangle, point, barycentric);
+    return ! (barycentric[0] < 0 || barycentric[1] < 0 || barycentric[2] < 0);
 }
 
-void drawLineTriangle(const Eigen::Vector2i &t0, const Eigen::Vector2i &t1, const Eigen::Vector2i &t2, TGAImage &image, const TGAColor &color) 
+void drawLineTriangle(const Eigen::Vector2i *triangle, TGAImage &image, const TGAColor &color) 
 { 
-    drawLine(t0, t1, image, color); 
-    drawLine(t1, t2, image, color); 
-    drawLine(t2, t0, image, color); 
+    drawLine(triangle[0], triangle[1], image, color); 
+    drawLine(triangle[1], triangle[2], image, color); 
+    drawLine(triangle[2], triangle[0], image, color); 
     std::cout << "triangle drawn" << std::endl;
 }
 
-// void drawFilledTriangle(Eigen::Vector2f t0, Eigen::Vector2f t1, Eigen::Vector2f t2, TGAImage &image, TGAColor color) 
-// {
+void drawFilledTriangle(const Eigen::Vector2i *triangle, TGAImage &image, const TGAColor &color) 
+{
+    // defines the bottom leftmost point of the bounding box
+    int boxMin[2] = {image.get_width() - 1, image.get_height() -1}; 
 
-// }
+    // defines the topmost point of the bounding box
+    int boxMax[2] = {0, 0}; 
+
+    // defines the upper bound of the image; triangle drawn outside this will be clipped
+    int upperClamp[2] = {image.get_width() - 1, image.get_height() -1};
+
+    for (int i=0; i<3; i++) {   // determine the bounding box parameters
+        for (int j=0; j<2; j++) {
+            boxMin[j] = std::max(0, std::min(boxMin[j], triangle[i][j]));
+            boxMax[j] = std::min(upperClamp[j], std::max(boxMax[j], triangle[i][j]));
+        }
+    }
+
+    std::cout << boxMin[0] << " " << boxMin[1]<< " " << boxMax[0]<< " " << boxMax[1] << std::endl;
+
+    Eigen::Vector2i point;
+    Eigen::Vector3f barycentric;
+    for (point[0]=boxMin[0]; point[0]<=boxMax[0]; point[0]++) {
+        for (point[1]=boxMin[1]; point[1]<=boxMax[1]; point[1]++) {
+            if (pointInTriangle(triangle, point, barycentric)) {
+                image.set(point[0], point[1], color);
+            }
+        }
+    }
+}
 
 #endif /* __UTILS_H__ */
