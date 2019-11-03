26034ab39446558808bd32c54a58b97617b63765
Tia Lim <limqiying19@gmail.com>
2019-11-03 01:12:44 -0500

    simple triangle drawer

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,13 @@
 #include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
-const int width = 800;
-const int height = 800;
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+const int width = 200;
+const int height = 200;
 Model *model = NULL;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) 
+void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) 
 {
     // uses Bresenham's Line Drawing Algorithm to draw a line from (x0, y0) to (x1, y1)
     bool steep = false;
@@ -50,12 +51,18 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
     }
 }
 
+void drawLine(Eigen::Vector2f p1, Eigen::Vector2f p2, TGAImage &image, TGAColor color) 
+{
+    drawLine(p1.x(), p1.y(), p2.x(), p2.y(), image, color);
+}
+
+
 void drawWireFrameMesh(const char* inputfile, const char* outputfile)
 {
     model = new Model(inputfile);
     TGAImage image(width, height, TGAImage::RGB);
 
-    // declare buffer variables for each line draw
+    // declare buffer variables for each drawLine draw
     Eigen::Vector3f v0, v1; 
     int x0, x1, y0, y1;
     std::vector<int> face;
@@ -88,7 +95,7 @@ void drawWireFrameMesh(const char* inputfile, const char* outputfile)
             // draw the line
             const float z = (1.0 - ((std::min(v0.z(), v1.z()) + std::abs(minZ)) / (maxZ - minZ))) * 255;
             const TGAColor lineColor = TGAColor(z, z, z, 255);
-            line(x0, y0, x1, y1, image, lineColor);
+            drawLine(x0, y0, x1, y1, image, lineColor);
         }
     }
 
@@ -97,7 +104,26 @@ void drawWireFrameMesh(const char* inputfile, const char* outputfile)
 
 }
 
+void drawTriangle(Eigen::Vector2f t0, Eigen::Vector2f t1, Eigen::Vector2f t2, TGAImage &image, TGAColor color) 
+{ 
+    drawLine(t0, t1, image, color); 
+    drawLine(t1, t2, image, color); 
+    drawLine(t2, t0, image, color); 
+    std::cout << "triangle drawn" << std::endl;
+}
+
 int main(int argc, char** argv) 
 {
-    drawWireFrameMesh("resources/head.obj", "output.tga");
+    // drawWireFrameMesh("resources/head.obj", "output.tga");
+    TGAImage image(width, height, TGAImage::RGB);
+
+    Eigen::Vector2f t0[3] = {Eigen::Vector2f(10, 70),   Eigen::Vector2f(50, 160),  Eigen::Vector2f(70, 80)}; 
+    Eigen::Vector2f t1[3] = {Eigen::Vector2f(180, 50),  Eigen::Vector2f(150, 1),   Eigen::Vector2f(70, 180)}; 
+    Eigen::Vector2f t2[3] = {Eigen::Vector2f(180, 150), Eigen::Vector2f(120, 160), Eigen::Vector2f(130, 180)}; 
+    drawTriangle(t0[0], t0[1], t0[2], image, red); 
+    drawTriangle(t1[0], t1[1], t1[2], image, white); 
+    drawTriangle(t2[0], t2[1], t2[2], image, green);
+
+    image.flip_vertically(); 
+    image.write_tga_file("output.tga");
 }
