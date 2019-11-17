5d1e3d58a3d054aeb9f7cba22abc393c912e2075
Tia Lim <limqiying19@gmail.com>
2019-11-16 19:31:27 -0500

    simplify modelView function

--- include/transformations.h
+++ include/transformations.h
@@ -22,39 +22,33 @@ void getModelView(Eigen::Vector3f eye, Eigen::Vector3f center, Eigen::Vector3f u
     x = up.cross(z).normalized();
     y = z.cross(x).normalized();
 
-    x.transpose();
-    y.transpose();
-    z.transpose();
-
-    Eigen::Matrix4f rotateToFrame = Eigen::Matrix4f::Identity();
-    Eigen::Matrix4f translateToFrame = Eigen::Matrix4f::Identity();
+    output = Eigen::Matrix4f::Identity();
 
     // construct matrix that will rotate the curremt coordinate frame into
     // the new coordinate frame
-    rotateToFrame.block<1,3>(0,0) = x.transpose();
-    rotateToFrame.block<1,3>(1,0) = y;
-    rotateToFrame.block<1,3>(2,0) = z;
+    for (int i=0; i<3; i++) {
+        output(0, i) = x[i];
+        output(1, i) = y[i];
+        output(2, i) = z[i];
+        output(i, 3)= -1 * center[i];
+    }
 
-    // construct matrix that translates from to the new coordinate system
-    translateToFrame.block<3, 1>(0, 4) = -1 * center;
-
-    output = rotateToFrame * translateToFrame;
 }
 
-void getViewport(float x, float y, int width, int height, Eigen::Matrix4f &output, int depth=255)
+void getViewport(float x, float y, int width, int height, Eigen::Matrix4f &output, int depth=255.0f)
 {
     // creates the viewport matrix that maps the bi-unit cube onto the screen
     // [-1, 1] ^ 3 -> [x, x+width] x [y, y+height] x [0, d]
-    output << width / 2.0, 0, 0, x + (width / 2.0),
-        0, height / 2.0, 0, y + (height / 2.0), 
-        0, 0, depth / 2.0, depth / 2.0,
-        0, 0, 0, 1;
+    output << width / 2.0f, 0, 0, x + (width / 2.0f),
+        0, height / 2.0f, 0, y + (height / 2.0f), 
+        0, 0, depth / 2.0f, depth / 2.0f,
+        0, 0, 0, 1.0f;
 }
 
 void getProjection(float c, Eigen::Matrix4f &output)
 {
     output = Eigen::Matrix4f::Identity();
-    output(3, 2) = -1.0 / c;
+    output(3, 2) = -1.0f / c;
 }
 
 Eigen::Vector4f toHomogenous(const Eigen::Vector3f &v)
