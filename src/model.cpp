0576c26807c44519ae08d57371b365991fbee749
Tia Lim <limqiying19@gmail.com>
2019-11-30 22:01:22 -0500

    remove shader struct

--- src/model.cpp
+++ src/model.cpp
@@ -134,10 +134,6 @@ Vector3f Model::vert(int i)
     return verts[i];
 }
 
-Vector3f Model::vert(int iface, int nthvert) {
-    return verts[faces[iface][nthvert][0]];
-}
-
 Vector2f Model::texture(int i) 
 {
     return textures[i];
